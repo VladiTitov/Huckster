563cd201ec677eb09e679ff7cdad1ac9e5fdde01
Vladislav Titov <capt.Hoffmann@gmail.com>
2022-04-11 22:36:17 +0300

    feat (dataAccess): added service registration extension

---
+++ src/Services/Parser/Parser.Infrastructure.DataAccess/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Parser.Infrastructure.DataAccess.Context;
+using Parser.Infrastructure.DataAccess.Interfaces;
+using Parser.Infrastructure.DataAccess.Repositories;
+
+namespace Parser.Infrastructure.DataAccess
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddDataAccessInfrastructure(this IServiceCollection services)
+            => services
+            .AddScoped<IAdsRepositoryAsync, AdsRepositoryAsync>()
+            .AddScoped<ApplicationDbContext>();
+    }
+}
