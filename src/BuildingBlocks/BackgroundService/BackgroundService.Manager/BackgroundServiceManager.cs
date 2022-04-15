40159a3569c6451d9bb9a1c85731d6b0ddfb26b2
Vladititov <capt.Hoffmann@gmail.com>
2022-04-15 16:38:20 +0300

    feat: added new implementation background service

---
+++ src/BuildingBlocks/BackgroundService/BackgroundService.Manager/BackgroundServiceManager.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CustomBackgroundService.Manager
+{
+    public abstract class BackgroundServiceManager : BackgroundService
+    {
+        private ILogger<BackgroundServiceManager> _logger;
+
+        public BackgroundServiceManager(ILogger<BackgroundServiceManager> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            if (this.ExecuteTask == null) return base.StartAsync(cancellationToken);
+            return Task.CompletedTask;
+        }
+
+        protected abstract void Execute(CancellationToken cancellationToken);
+
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    Execute(cancellationToken);
+                    await Task
+                        .Delay(15000, cancellationToken)
+                        .ConfigureAwait(false);
+                }
+            }
+            catch (TaskCanceledException ex)
+            {
+                _logger.LogInformation($"{ex.Message} {DateTime.Now}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{ex.Message} {DateTime.Now}");
+            }
+        }
+    }
+}
